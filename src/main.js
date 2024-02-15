5341a25536d783903f389df6cc4fcda40a957c32
Marcelo Giati <mtgiati@gmail.com>
2024-02-15 17:12:37 -0300

    sprint02

---
+++ src/main.js
@@ -0,0 +1,46 @@
+// eslint-disable-next-line no-undef
+const navbar = document.getElementById("navbar");
+
+// eslint-disable-next-line no-undef
+window.addEventListener("scroll", () => {
+
+   // eslint-disable-next-line no-undef
+   const scrollposition = window.scrollY;
+   const threshold = 0; 
+   if(scrollposition > threshold){
+      navbar.style.backgroundColor = "#114250";
+   } else { 
+      navbar.style.backgroundColor = "transparent"; 
+   }
+})
+
+// const contAnimation = () => {
+//    const experienceElement = document.getElementById("experience");
+//    const clientsElement = document.getElementById("clients");
+//    const projectsElement = document.getElementById("projects");
+//    const screensElement = document.getElementById("screens");
+
+//    const obj = {
+//       ex: [0, 3],
+//       cl: [0, 20],
+//       pr: [0, 50],
+//       sc: [0, 200],
+//    };
+
+//    setInterval(() => {
+
+//       obj.ex[0] = obj.ex[0] < obj.ex[1] ? obj.ex[0] + 1 : obj.ex[1];
+//       obj.cl[0] = obj.cl[0] < obj.cl[1] ? obj.cl[0] + 1 : obj.cl[1];
+//       obj.pr[0] = obj.pr[0] < obj.pr[1] ? obj.pr[0] + 1 : obj.pr[1];
+//       obj.sc[0] = obj.sc[0] < obj.sc[1] ? obj.sc[0] + 1 : obj.sc[1];
+
+//       experienceElement.textContent = `${obj.ex[0]} +`;
+//       clientsElement.textContent = `${obj.cl[0]} +`;
+//       projectsElement.textContent = `${obj.pr[0]} +`;
+//       screensElement.textContent = `${obj.sc[0]} +`;
+
+//    }, 10);
+// }
+
+// window.addEventListener('load', contAnimation)
+
